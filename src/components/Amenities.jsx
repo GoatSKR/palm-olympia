9cc8c228a7500654d92b26cb8d759ec91942a8b1
GoatSKR <sitesh.raulo.pj@gmail.com>
2024-02-02 18:21:34 +0530

    frontend - amenities price form layout and logo added. Please check for responsiveness

---
+++ src/components/Amenities.jsx
@@ -0,0 +1,25 @@
+export default function Amenities() {
+    return (
+        <div className="bg-[#F1F1F1] py-[8%] px-[8%]">
+            <div>
+                <h1 className="text-black text-center text-4xl" > AMENITIES</h1>
+
+            </div>
+            <div>
+                <p className="text-black mt-[1%] text-center text-2xl">Sit-out courts | Sculpture court | Lawn | Fitness court | Yoga Court | Amphitheatre | Kid’s Play Area | Pool deck Kids pool | Swimming Pool | Grand club house | Skating Rink | Multi-Purpose court | Jogging Track</p>
+            </div>
+            <div className="mx-[8%] mt-[4%] grid grid-cols-1 md:grid-cols-3 gap-9">
+                <img className="w-auto h-auto rounded-lg" src="../src/assets/images/amenities/img106.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img92.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img94.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img176.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img122.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img118.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img82.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img104.jpg"/>
+                <img className="w-auto h-auto rounded-md" src="../src/assets/images/amenities/img116.jpg"/>
+
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Floorplan.jsx
@@ -0,0 +1,16 @@
+export default function Floorplan() {
+    return (
+        <div className="py-[5%]"> 
+            <div className="text-4xl text-center">
+                <h1> MASTER & FLOOR PLAN </h1>
+            </div>
+            <div className="mx-[8%] my-[8%] grid grid-cols-1 md:grid-cols-2 gap-9">
+                <img className="w-auto h-auto" src="../src/assets/images/plan1.jpg"/>
+                <img className="w-auto h-auto" src="../src/assets/images/plan2.jpg"/>
+                <img className="w-auto h-auto" src="../src/assets/images/plan3.jpg"/>
+                <img className="w-auto h-auto" src="../src/assets/images/plan4.jpg"/>
+            </div>
+            
+        </div>
+    )
+}
